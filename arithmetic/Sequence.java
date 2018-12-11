daa4a41ed3ed6118332dc6a6c6579c231349b10a
changha0321 <changha0321@naver.com>
2018-12-11 16:42:08 +0900

    2018.12.11.04:41

---
+++ arithmetic/Sequence.java
@@ -0,0 +1,28 @@
+package arithmetic;
+
+import java.util.Scanner;
+
+/**
+ 등차수열 = 연속하는 두 항의 차이가 모두 일정한 수열
+ 공차 = 두 항들에 대해서 공통적으로 나타나는 차이므로, 공차
+ 2+8+14+20+26+32=
+ * */
+public class Sequence {
+	public static void main(String[] args) {
+	
+		int a = 0;
+		int sum = 2;
+		String b ="";
+		for(int i=0; i< 5;i++) {
+			i = 6;
+			b += 2+i;
+			sum += i;
+			
+				
+			}
+		}
+			
+		
+		
+	}
+}
