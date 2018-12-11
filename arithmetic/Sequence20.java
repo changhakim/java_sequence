49e9c28cd6ec43dbb4607c17d9e7e2ffd7d9c91a
changha0321 <changha0321@naver.com>
2018-12-11 19:45:51 +0900

    2018.12.11.0745

---
+++ arithmetic/Sequence20.java
@@ -0,0 +1,29 @@
+package arithmetic;
+
+public class Sequence20 {
+	public static void main(String[] args) {
+		 int i = 0, d = 1, sum = 1;
+
+	        while (true) {
+	           // 딱3줄 이곳을 채우시오
+	        	i++;
+	        	if(i<20) {
+	        		d =  d+i;
+	        		sum = sum + d;
+	        		
+	        		continue;
+	        	}else {
+	        		break;
+	        	}
+	        }
+	        System.out.println(sum);
+	        /*System.out.printf(
+	                  "======================================================\n"
+	                + "1+2+4+7+11+16+22+... 증가하는 수열의 20번째 항까지 합 \n"
+	                + "======================================================\n"
+	                + "sum \n"
+	                + "=======================================================",sum);*/
+
+		
+	}
+}
---
+++ arithmetic/Sequence5.java
@@ -0,0 +1,46 @@
+package arithmetic;
+
+/**
+등차수열: 연속하는 두 항의 차이가 모두 일정한 수열
+공차(common difference) : 공통적으로 나타나는 차이
+2+8+14+20+26=70
+ * */
+public class Sequence5 {
+	public static void main(String[] args) {
+		int A = 2; // 초항 2
+        int S = A;
+        int D = 6; // 공차
+        int N = 2; // 번째
+        int AN = 0;
+        // 2 + 8 + 14 + 20
+        // 2 + (2+6) + (2+6+6)+ (2+6+6+6)
+        String ex = "2";
+        String gg ="";
+        while (true) {
+        	N++;
+        	if(N<6) {
+        		A=A+D;
+        		gg += A+"+"; 
+        		System.out.println(A+"의변화");
+        		 S = S+A;
+        		 
+        		 System.out.println(S+"의변화A");
+        	continue;
+        	}else {
+        		A=A+D;
+        		gg += A+"="; 
+        		System.out.println(A+"의변화");
+        		 S = S+A;
+        		 
+        		 System.out.println(S+"의변화A");
+        		break;
+        	
+        	}
+        	
+        	
+        }
+        System.out.println(ex +"+"+gg+ S);
+        // System.out.println(ex + "=" + S); // syso
+		
+	}
+}
